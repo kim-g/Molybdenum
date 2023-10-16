daa340d12179ee65fd2a9fdc132b45aa249da752
Григорий Ким <kim-g@yandex.ru>
2023-10-16 13:35:07 +0500

    Добавьте файлы проекта.

---
+++ Molybdenum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molybdenum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Список результатов.
+            // Комментарий переводчика. Понятия не имею, зачем тут нужен трёхмерный массив, по всем данным достаточно двумерного.
+            // Я же использую одномерный список (массив будет в C# статичным, то есть с заранее заданным размером, а нам оно не надо) элементов класса Point.
+            List<Point> Memory = new List<Point>();
+
+
+            // Создаём массив и заполняем его нулями. Пока оставим, потом, скорее всего, доделаю в класс.
+        }
+    }
+}
