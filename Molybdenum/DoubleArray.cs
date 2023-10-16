ded306947414faa217d08f0147258c0c2a664b21
Григорий Ким <kim-g@yandex.ru>
2023-10-16 13:51:54 +0500

    Создание особого массива

---
+++ Molybdenum/DoubleArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molybdenum
+{
+    /// <summary>
+    /// Сложный двумерный массив с вункциями сумм по рядам и полной суммы
+    /// </summary>
+    internal class DoubleArray
+    {
+        private byte[,] data = new byte[12,12] 
+        {
+            {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, 
+            {1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, 
+            {1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0}, 
+            {1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0}, 
+            {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0}, 
+            {1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1}, 
+            {0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0}, 
+            {0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0}, 
+            {0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1}, 
+            {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1}, 
+            {0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1}, 
+            {0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0}
+        };
+
+        /// <summary>
+        /// Данные массива
+        /// </summary>
+        public byte[,] Data 
+        { 
+            get => data; 
+            set => data=value; 
+        }
+
+    }
+}
